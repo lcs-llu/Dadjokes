8371c8af6c9614cf37570cf1a47e70fd4359ef76
Leo Lu <llu@lcs.on.ca>
2022-02-22 11:19:38 -0500

    Initial Commit

---
+++ Dadjokes/DadjokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadjokesApp.swift
+//  Dadjokes
+//
+//  Created by Leo Lu on 2022-02-22.
+//
+
+import SwiftUI
+
+@main
+struct DadjokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
